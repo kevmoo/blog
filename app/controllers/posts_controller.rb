a33ac49f2012dbaf834d19827a5f479e878d1536
Kevin Moore <kevin@thinkpixellab.com>
2010-11-01 12:29:26 -0700

    Fun w/ scratch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,12 @@ class PostsController < ApplicationController
     respond_with(@post = Post.find(params[:id]))
   end
 
+  def new_scratch
+  end
+
+  def scratch
+    # make sure there is a scratch.haml file, right?
+    render :file => File.join(Rails.root, 'tmp', 'scratch', 'scratch.haml')
+  end
+
 end
