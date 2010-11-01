e1633601ac1577fbbbb4936dc154f269d46df914
Kevin Moore <kevin@thinkpixellab.com>
2010-11-01 12:29:03 -0700

    Updated the model

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,7 +1,13 @@
 class Post
-  include Mongoid::Document  
-  include Mongoid::Versioning  
-  
-  field :title  
-  field :content  
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Versioning
+
+  field :title
+  field :content
+  field :posted_at, :type => Date
+  field :is_draft, :type => Boolean
+
+  index :posted_at
+  index :title
 end
