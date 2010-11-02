1c0ef21324e2f53730068a5f1f0b1a234d56ec90
Kevin Moore <kevin@thinkpixellab.com>
2010-11-01 20:53:40 -0700

    things working better

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,30 @@ class PostsController < ApplicationController
     respond_with(@post = Post.find(params[:id]))
   end
 
+  def new
+    # handle errors...
+  end
+
   def new_scratch
+    @post = Post.new
     @file_path = Scratch.haml_path
+  end
+
+  def create
+    @post = Post.new(params[:space])
+
+    content = ''
+
+    File.open(Scratch.haml_path, mode = 'r') do |file|
+      content = file.read
+      content = Haml::Engine.new(content).render
+    end
+
+    @post.content = content
+    @post.save!
+
+    redirect_to :root
+
   end
 
   def scratch
