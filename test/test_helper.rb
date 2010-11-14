6811288592b20716fddc5f73369a5c55252dc9af
Kevin Moore <kevin@thinkpixellab.com>
2010-11-13 17:03:18 -0800

    No mongo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,16 +3,4 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  teardown :clean_mongodb
-
-
-  def clean_mongodb
-    #puts "cleaning mongodb...."
-    Mongoid.database.collections.each do |collection|
-      unless collection.name =~ /^system\./
-        collection.remove
-      end
-    end
-    #puts "finished cleaning mongodb."
-  end
 end
