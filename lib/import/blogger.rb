8c5e09fbc3c47a7bd685c89b856db618b94d04e2
Kevin Moore <kevin@thinkpixellab.com>
2010-11-21 22:34:30 -0800

    blogger fun...

--- lib/import/blogger.rb
+++ lib/import/blogger.rb
@@ -1,19 +1,30 @@
 module Import
-  module Blogger
+  class Blogger
 
-    def self.get_entries(filename)
+    XMLNS = {:atom => 'http://www.w3.org/2005/Atom'}
 
+    def initialize(blogger_export_xml_path)
+      @doc = get_nokogiri_doc(blogger_export_xml_path)
     end
 
+    def save
+      entries.each do |entry|
+        Blob.get(entry.to_xml)
+      end
+    end
+
+    def entries
+      @doc.xpath("//atom:entry", 'atom' => XMLNS[:atom])
+    end
 
     private
 
-    def self.get_nokogiri_doc(filename)
+    def get_nokogiri_doc(filename)
       content = open_file(filename)
       Nokogiri::XML(content)
     end
 
-    def self.open_file(filename)
+    def open_file(filename)
       File.open(filename, 'r') do |file|
         return file.read
       end
@@ -21,46 +32,3 @@ module Import
 
   end
 end
-
-
-
-def get_posts(xs)
-  posts = []
-
-  xs['entry'].each do |entry|
-    categories = entry['category']
-
-    if categories.any?{ |category| category['term'] == 'http://schemas.google.com/blogger/2008/kind#post'}
-
-      np = {}
-      np['tags'] = entry['category'].find_all{ |cat| cat['term'][0,5] != 'http:'}.collect{ |cat| cat['term'] }
-      np['title'] = entry['title'][0]['content']
-      np['published'] = DateTime.parse(entry['published'][0]).utc.to_s
-      np['id'] = entry['id'][0]
-      np['content'] = entry['content']['content']
-
-      link = entry['link'].find{ |link| link['rel'] == 'alternate'}
-      if(link != nil)
-        np['original_url'] = link['href']
-      end
-
-      posts << np
-    end
-
-  end
-
-  return posts
-
-end
-
-#posts = get_posts(xs)
-
-def save_hash_to_json(obj, file_name)
-  File.open(file_name, 'w') do |file|
-
-    file.write obj.to_json
-
-  end
-end
-
-#save_hash_to_json(posts, "#{file_name}.posts.json")
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,15 +1,30 @@
 require File.join(Rails.root, 'lib/import/blogger.rb')
 
-namespace :import do
-  desc "Import posts from a blogger blog xml export"
-  task :blogger => :environment do
+def parse(entry)
+  puts entry.inspect
+end
 
-    filename = ENV['IMPORT_FILE']
-    unless filename
-      filename = '/Users/kevin/source/github/myblog/content/personal_blog-11-21-2010.xml'
+namespace :import do
+
+  namespace :blogger do
+
+    desc "Import posts from a blogger blog xml export"
+    task :import => :environment do
+
+      filename = ENV['IMPORT_FILE']
+      unless filename
+        filename = '/Users/kevin/source/github/myblog/content/personal_blog-11-21-2010.xml'
+      end
+
+      Import::Blogger.new(filename).save
+
     end
 
-    reader =  Import::Blogger.get_nokogiri_doc(filename)
+    desc "Posts for blobs"
+    task :to_posts => :environment do
+
+    end
 
   end
+
 end
