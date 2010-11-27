c8609003ca82b7f6ebe0506383a71dfd77c43255
Kevin Moore <kevin@thinkpixellab.com>
2010-11-26 22:04:16 -0800

    More fun...

--- spec/lib/blogger_import_spec.rb
+++ spec/lib/blogger_import_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
+include Import
+
+TEST_FILES = Dir.glob(File.expand_path('../../data/blogger/*', __FILE__))
 
 describe Import::Blogger do
+  TEST_FILES.each do |file|
+    describe "for file #{file}" do
 
+      describe "should import awesome" do
+        blogger = Blogger.new(file)
+
+        blogger.entries.select{ |entry| Blogger.is_post?(entry.to_xml) }.each do |entry|
+          count = 0
+          it "entry number #{count} should be cool" do
+            puts CGI.escapeHTML(entry.to_xml)
+          end
+          count += 1
+        end
+      end
+    end
+  end
 end
