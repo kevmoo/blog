5cad980eb7acc12dfe6f137d5b72873b8a984f11
Kevin Moore <kevin@thinkpixellab.com>
2010-11-29 10:03:50 -0800

    More progress on version

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,10 +1,18 @@
 class Version < ActiveRecord::Base
   serialize :metadata, Hash
   belongs_to :blob, :readonly => true
-  has_one :previous, :class_name => "Version", :foreign_key => "previous_id"
+  belongs_to :previous, :class_name => 'Version', :foreign_key => 'previous_id'
+  attr_readonly :blob_id, :previous_id
   validates :blob_id, :presence => true
+  validate :good_changes
 
-  before_validation(:on => :save) do
-    self.metadata ||= {}
+  private
+
+  def good_changes
+    if persisted?
+      changes.keys.select{ |attrib| Version.readonly_attributes.include?(attrib)}.each do |attrib|
+        errors[attrib] << "Cannot change after it's been saved"
+      end
+    end
   end
 end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -6,7 +6,18 @@ describe Version do
   end
 
   it 'should allow valid' do
-    v = Version.new(:blob => Blob.create(:value => "foo"))
+    v = Version.new(:blob => Blob.get('foo'))
     v.save!
+  end
+
+  it 'should allow versioning coolness' do
+    v = Version.create(:blob => Blob.get('foo'))
+    v.id.should_not eq(nil)
+
+    v2 = Version.create(:blob => Blob.get('bar'), :previous => v)
+    v2.previous_id.should eq(v.id)
+
+    v2.previous_id = nil
+    v2.save.should be_false
   end
 end
