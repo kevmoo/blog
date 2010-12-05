78702c302d703289691de357f38ad7b05d8c58f6
Kevin Moore <kevin@thinkpixellab.com>
2010-12-05 15:33:33 -0800

    Removing default xml output for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.xml
   def index
     @posts = Post.active
     if params[:year] && params[:month]
@@ -10,85 +8,48 @@ class PostsController < ApplicationController
     end
 
     @posts = @posts.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @posts }
-    end
   end
 
-  # GET /posts/1
-  # GET /posts/1.xml
   def show
     if params[:id]
       @post = Post.find(params[:id])
     else
       @post = Post.month(params[:year].to_i, params[:month].to_i).find_by_slug(params[:slug])
     end
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
   end
 
-  # GET /posts/new
-  # GET /posts/new.xml
   def new
     @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
   end
 
-  # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.xml
   def create
     @post = Post.new(params[:post])
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to(@post, :notice => 'Post was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.xml
   def update
     @post = Post.find(params[:id])
 
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
+    if @post.update_attributes(params[:post])
+      redirect_to(@post, :notice => 'Post was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.xml
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(posts_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(posts_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Blog::Application.routes.draw do
 
-  resources :posts
+  # resources :posts
 
   match "/:year/:month/:slug" => "posts#show", :constraints => { :year => /\d{4}/, :month => /\d{2}/ }
   match "/:year(/:month)" => "posts#index", :constraints => { :year => /\d{4}/, :month => /\d{2}/ }
