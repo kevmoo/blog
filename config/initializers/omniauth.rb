1d57c0ca913210be7b54f0adedd46566a3177fed
Kevin Moore <kevin@thinkpixellab.com>
2010-12-09 17:21:55 -0800

    testing for twitter user, better testing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 require 'omniauth/core'
 require 'omniauth/oauth'
 
-throw 'twitter keys not defined!' unless ENV.key?('TWITTER_CONSUMER_KEY') && ENV.key?('TWITTER_CONSUMER_SECRET')
+%w(TWITTER_CONSUMER_KEY TWITTER_CONSUMER_SECRET TWITTER_USER).each do |key|
+  throw "a required environment var is not set: #{key}" unless ENV.key?(key)
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
