1b37865d730c19af713c086906e58d9bce2c5607
Kevin Moore <kevin@thinkpixellab.com>
2010-12-10 22:08:26 -0800

    Better data with auth + flash

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,9 +1,15 @@
 class AuthController < ApplicationController
   def twitter_callback
     omni_hash = request.env['omniauth.auth']
-    puts omni_hash.inspect
-    if omni_hash && omni_hash['uid'] == ENV['TWITTER_UID']
-      session[:user] = omni_hash['user_info']
+    if omni_hash
+      nick = omni_hash['user_info']['nickname']
+      if omni_hash['uid'] == ENV['TWITTER_UID']
+        session[:user] = omni_hash['user_info']
+        flash[:notice] = "Welcome, #{nick}"
+      else
+        session.delete(:user)
+        flash[:notice] = "Sorry #{nick}. I don't know you"
+      end
     end
     redirect_to :root
   end
